da1f8f48f35454e6c7c368b4a270fbeb4112db59
budkin86bk <budkin86bk@yandex.ru>
2024-03-10 09:42:01 +0500

    module4-task1

---
+++ js/main.js
@@ -0,0 +1,71 @@
+const MESSAGES = ['Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Иван',
+  'Артём',
+  'Мария',
+  'Санёк',
+  'Виктор',
+  'Юлия',
+  'Лолита',
+  'Пётр',
+];
+
+const MAX_NUMBER_OF_PHOTOS = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MIN_COMMENTS = 0;
+const MAX_COMMENTS = 30;
+const MIN_AVATAR_NUMBER = 1;
+const MAX_AVATAR_NUMBER = 6;
+
+const getRandomNumber = (from, before) => {
+  const lower = Math.ceil(Math.min(from, before));
+  const upper = Math.floor(Math.max(from, before));
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
+};
+
+const getRandomArrayElement = (array) => array[getRandomNumber(0, array.length - 1)];
+
+const counterPhoto = (() => {
+  let count = 1;
+  return function() {
+    return count++;
+  };
+})();
+
+const counterComment = (() => {
+  let count = 1;
+  return function() {
+    return count++;
+  };
+})();
+
+const createComment = () => {
+  const id = counterComment();
+  return {
+    id,
+    avatar: `img/avatar-${getRandomNumber(MIN_AVATAR_NUMBER, MAX_AVATAR_NUMBER)}.svg`,
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES)
+  };
+};
+
+const createPhoto = () => {
+  const id = counterPhoto();
+  return {
+    id,
+    url: `photos/${id}.jpg`,
+    description: `Это фотография #${id}`,
+    likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+    commens: Array.from({length: getRandomNumber(MIN_COMMENTS, MAX_COMMENTS)}, createComment)
+  };
+};
+
+const photos = Array.from({length: MAX_NUMBER_OF_PHOTOS}, createPhoto);
