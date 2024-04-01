3450ebb652a677ef75e0a13304bdff6fceab55c1
budkin86bk <budkin86bk@yandex.ru>
2024-04-01 12:10:15 +0500

    изменения 01_04

--- js/main.js
+++ js/main.js
@@ -8,11 +8,16 @@ renderThumbnails(photos);
 const photoConteiner = document.querySelector('.pictures');
 const photoFullContainer = document.querySelector('.big-picture');
 const photoFullImg = photoFullContainer.querySelector('.big-picture__img').querySelector('img');
+const photoFullLikeCount = photoFullContainer.querySelector('.likes-count');
+const photoFullUlCount = document.querySelector('.social__comments').children;
+const photoFullCommentShowCount = photoFullContainer.querySelector('.social__comment-shown-count');
+const photoFullCommentTotalCount = photoFullContainer.querySelector('.social__comment-total-count');
 
 
 const openFullPhoto = (photoId) => {
   const photoCurrent = photos.find((photo) => photo.id === Number(photoId));
   const commentFragment = document.createDocumentFragment();
+
 };
 
 
@@ -22,6 +27,11 @@ photoConteiner.addEventListener('click', (evt) => {
   if (currentPhotoNode) {
     photoFullContainer.classList.remove('hidden');
   }
+  photoFullImg.src = photos[0].url;
+  photoFullLikeCount.textContent = photos[0].likes;
+  photoFullCommentShowCount.textContent = photoFullUlCount.length;
+  photoFullCommentTotalCount.textContent = photos[0].commens.length;
+
 });
 
 
